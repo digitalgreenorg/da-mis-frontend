d47e3339b33e86cc5dbd764b92af496530434449
Mayank Gautam <mgautam099@gmail.com>
2020-06-20 23:47:28 +0530

    location access form

--- kpi/admin.py
+++ kpi/admin.py
@@ -3,10 +3,13 @@ from django.contrib import admin
 
 from hub.models import ExtraUserDetail
 from .models import AuthorizedApplication
+from .models import LocationAccess
+from .models.geography import LocationAccessAdmin
 
 # Register your models here.
 admin.site.register(AuthorizedApplication)
 admin.site.register(ExtraUserDetail)
+admin.site.register(LocationAccess, LocationAccessAdmin)
 admin.site.site_header = 'DA/SMS Administration'
 admin.site.site_title = 'Admin'
 admin.site.index_title = 'DA/SMS'
--- kpi/models/geography.py
+++ kpi/models/geography.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.conf import settings
+from django import forms
+from django.contrib import admin
 
 
 class Region(models.Model):
@@ -50,3 +53,30 @@ class Kebele(models.Model):
 
     class Meta:
         unique_together = ("name", "woreda")
+
+
+class LocationAccess(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL,
+                                related_name='extra_details',
+                                on_delete=models.CASCADE)
+    regions = models.ManyToManyField(Region)
+    zones = models.ManyToManyField(Zone)
+    woredas = models.ManyToManyField(Woreda)
+    kebeles = models.ManyToManyField(Kebele)
+
+
+class LocationAccessForm(forms.ModelForm):
+    class Meta:
+        model = LocationAccess
+
+    def __init__(self, *args, **kwargs):
+        super(LocationAccessForm, self).__init__(*args, **kwargs)
+        self.fields['zones'].queryset = Zone.objects.filter(region__in=self.instance.regions)
+        self.fields['woredas'].queryset = Woreda.objects.filter(zone__in=self.instance.zones)
+        self.fields['kebeles'].queryset = Kebele.objects.filter(woreda__in=self.instance.woredas)
+
+
+class LocationAccessAdmin(admin.ModelAdmin):
+    form = LocationAccessForm
+    filter_horizontal = ['zones', 'woredas', 'kebele']
+
